5cfa85249cf15618ec0c6143de700c52897af86c
dharshan-kumarj <jdharshankumar18@gmail.com>
2024-04-18 00:20:58 +0530

    contact page started

--- _libs/_templates/_contact.php
+++ _libs/_templates/_contact.php
@@ -1,7 +1,22 @@
-<main class="px-3">
-    <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-    <p class="lead">
-      <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
-    </p>
-  </main>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content="width=devicei-width, initial scale=1.0">
+    <title>Contact Form</title>
+</head>
+<link rel="stylesheet" href="/Arun_Portfolio/_libs/assets/dist/css/contact.css">
+<body>
+    <div class="contact-container">
+        <form action="" class="contact-left">
+            <div class="contact-left-title">
+                <h1>Get in touch</h1>
+                <hr>
+            </div>
+            <input type="text" name="name" plceholder="Your Name" class="contact-inputs" required>
+            <input type="email" name="email" plceholder="Your Email" class="contact-inputs" required>
+            <textarea name="message" placeholder="Your message" class="contact-inputs" required></textarea>
+            <button type="submit"> Submit</button>
+        </form>
+    </div>
+</body>
+</html>
